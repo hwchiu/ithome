90acb8a94dc56616e2ab586ee6ccc9561b409b67
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-09-05 22:45:23 -0700

    add sitemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
+const sitemap = require('gulp-sitemap');
 
 const $ = gulpLoadPlugins();
+
+gulp.task('sitemap', function () {
+   return gulp.src('./_book/*/**/*.html', {
+            read: false
+        })
+        .pipe(sitemap({
+            siteUrl: 'http://ithome.hwchiu.com'
+        }))
+        .pipe(gulp.dest('./'));
+});
 
 gulp.task('publish', () => {
   console.log('Publish Gitbook (_book) to Github Pages');
