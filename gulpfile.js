8b49f2a42e672e7c3712fbc64118bdad97bbdf3e
Hung-Wei Chiu <hungwei@opennetworking.org>
2020-09-05 22:47:44 -0700

    Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,16 @@ const sitemap = require('gulp-sitemap');
 
 const $ = gulpLoadPlugins();
 
-gulp.task('sitemap', function () {
-   return gulp.src('./_book/*/**/*.html', {
+gulp.task('publish', () => {
+  console.log('Publish Gitbook (_book) to Github Pages');
+  gulp.src('./_book/*/**/*.html', {
             read: false
         })
         .pipe(sitemap({
             siteUrl: 'http://ithome.hwchiu.com'
         }))
-        .pipe(gulp.dest('./'));
-});
+        .pipe(gulp.dest('./_book/'));
 
-gulp.task('publish', () => {
-  console.log('Publish Gitbook (_book) to Github Pages');
   return gulp.src('./_book/**/*')
     .pipe($.ghPages({
       origin: 'origin',
